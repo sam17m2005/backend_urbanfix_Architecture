31aa56165e2412654d00430cfcf97803352c90f0
Johan Felipe Aguilar Castillo <141509764+FelipeAguilar302@users.noreply.github.com>
2025-09-30 11:04:26 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-# Se crea la instancia de la base de datos sin vincularla a una app todavía
 db = SQLAlchemy()
 
 def create_app():
@@ -10,10 +9,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    # Inicializa la base de datos con la configuración de la app
     db.init_app(app)
     
-    # Importar y registrar las rutas (Blueprints)
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
